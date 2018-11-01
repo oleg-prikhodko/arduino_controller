2f7d79a1e5144c9807a383f370a1758cfb8b0e98
Oleg <mightyxerox@gmail.com>
2018-11-01 11:15:34 +0300

    Get rid of sleep function

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from time import sleep
 import serial
 
 
 with serial.Serial(port='COM3', baudrate=9600, timeout=1) as serial_port:
     while True:
-        # rotation = int.from_bytes(serial_port.readline(), byteorder='little')
         try:
             rotation = int(serial_port.readline().decode("ascii"))
+            print(rotation)
         except ValueError:
             continue
-            
-        print(rotation)
-        # sleep(.1)
